9f0d6fe9f6a122a907d530be6caf1f8af1952c8a
Brayan Muniz <brayanMuniz@users.noreply.github.com>
2023-05-26 14:46:31 -0500

    Background color with text content

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 export default function Home() {
   return (
     <main className="">
-      <div className="flex h-screen bg-gray-100">
-        <div className="w-1/6 bg-white p-4">
-          <h1 className="text-xl">Left Sidebar</h1>
-          {/* Your left column content goes here */}
+      <div className="flex h-screen dark:bg-[#343541]">
+        <div className="w-1/6 dark:bg-[#202123] p-4">
+          <h1 className="text-xl">Grammar Questions</h1>
+          <h1 className="text-xl">Mock Test</h1>
+          <h1 className="text-xl">Vocab</h1>
         </div>
-        <div className="w-4/6 bg-gray-200 p-4">
+
+        <div className="w-4/6 dark:bg-[#343541] p-4">
           <h1 className="text-xl">Main Content</h1>
-          {/* Your main column content goes here */}
         </div>
-        <div className="w-1/6 bg-white p-4">
-          <h1 className="text-xl">Right Sidebar</h1>
-          {/* Your right column content goes here */}
+
+        <div className="w-1/6 dark:bg-[#202123] p-4 flex flex-col justify-between">
+          <div>
+            <div> Scenarios</div>
+            <div>Anki Generation</div>
+          </div>
+
+          <div>
+            <div>Export</div>
+            <div>Account</div>
+            <div>Open AI Key</div>
+          </div>
         </div>
       </div>
     </main>
